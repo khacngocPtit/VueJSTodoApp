f524888fc4494edf47943c99d56b0ee2d9ed04c9
khacngocPtit <khacngocit@gmail.com>
2019-12-28 16:15:59 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <todolist/>
+  </div>
+</template>
+
+<script>
+import todolist from './components/Todolist'
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components:{
+    todolist,
+  }
+}
+</script>
+<style>
+
+</style>
